f310acb8c045484048fce006d6a9e064ec9dc330
alinachudaeva <ac@axiros.com>
2021-06-11 12:03:40 +0300

    winston

--- server/index.js
+++ server/index.js
@@ -2,11 +2,29 @@ const { response } = require('express');
 require('dotenv').config({path: '../.env'});
 
 const http = require('http');
+const winston = require('winston');
 
 const port = process.env.PORT;
 
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error'}),
+        new winston.transports.File({ filename: 'combined.log', maxsize: 500 }),
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
+
+
+
 http.createServer((request, response)=>{
-    
     switch(request.url){
         case('/home'):
             response.write('home page');
@@ -25,9 +43,8 @@ http.createServer((request, response)=>{
     response.end()
 }).listen(port, (error) => {
     if (error) {
-        console.log(error);
+        logger.error('Something went wrong! error: ' + error)
     }else {
-        console.log('API running on localhost:' + port)
+        logger.info('API running on localhost:' + port)
     }
-    
 })
